ee9219a6fdd9ec55350b8fdf4285ad13c85093eb
oktoberf <bollbravl@gmail.com>
2022-12-01 19:05:58 +0300

    main game's file update

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+# Import moduls
 import pygame
 
-
-class Game:
-    def __init__(self):
+# Classes
+class Game: # Game's class
+    def __init__(self): # Game's initialization
         super().__init__()
-        pygame.init()
-        self.window_size = (1200, 800)
-        self.screen = pygame.display.set_mode(self.window_size)
+        pygame.init() # Pygame's initialization
+        self.window_size = (1200, 800) # Window size
+        self.screen = pygame.display.set_mode(self.window_size) # Screen
 
-    def update(self):
+    def update(self): # Update the game
         self.screen.fill((0, 0, 0))
         pygame.display.flip()
-    def event_handler(self):
+    def event_handler(self): # Check Event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
+    def run(self):
+        while True:
+            self.update()
+
 if __name__ == '__main__':
     game = Game()
-    while True:
-        game.update()
+    game.run() # Game's run
