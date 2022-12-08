ce02f2126f136c18cc150142f70460f7dca3305b
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-12-08 22:03:00 +0300

    new branch

--- panda3D/main.py
+++ panda3D/main.py
@@ -7,17 +7,17 @@ from direct.task import Task
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        self.scene = self.loader.loadModel("models/roads.obj")
+        self.scene = self.loader.loadModel("models/roads2.obj")
         self.scene.getPos()
         self.scene.reparentTo(self.render)
         self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(1, 1, 1)
+        self.scene.setPos(0, 0, 0)
 
         # Define a procedure to move the camera.
         def spinCameraTask(self, task):
             angleDegrees = task.time * 6.0
             angleRadians = angleDegrees * (pi / 180.0)
-            self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+            self.camera.setPos(5, 0, 0)
             self.camera.setHpr(angleDegrees, 0, 0)
             return Task.cont
 
