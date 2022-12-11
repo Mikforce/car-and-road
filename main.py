4d4b7d8913e91c9adfba2b1cb56f541cd56a7dd0
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-12-11 20:42:20 +0300

    new branch

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ class Game(ShowBase):
         ShowBase.__init__(self)
         self.scene = self.loader.loadModel("models/roads2.obj")
         self.car = self.loader.loadModel("models/car.obj")
-        self.scene.getPos()
         self.scene.reparentTo(self.render)
+        self.car.reparentTo(self.render)
         self.scene.setScale(0.25, 0.25, 5)
         self.scene.setPos(0, 6.5, -1)
-        self.scene.setHpr(0, 90, 0)
+        self.scene.setHpr(90, 0, 90)
+        self.car.setScale(0.25, 0.25, 0.25)
+        self.car.setPos(0, 6.5, -1)
 
 if __name__ == '__main__':
     game = Game()
