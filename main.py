d71afd9f526a63f72f7bf04e49e0c41c4e8bacd8
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-12-11 20:46:05 +0300

    new branch

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from direct.showbase.ShowBase import ShowBase
 import panda3d
+import fbx
 
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.scene = self.loader.loadModel("models/roads2.obj")
-        self.car = self.loader.loadModel("models/car.obj")
+        self.car = self.loader.loadModel("models/car.fbx")
         self.scene.reparentTo(self.render)
         self.car.reparentTo(self.render)
+        self.car.ls()
+        self.car.list_joints()
         self.scene.setScale(0.25, 0.25, 5)
         self.scene.setPos(0, 6.5, -1)
         self.scene.setHpr(90, 0, 90)
