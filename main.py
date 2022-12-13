4bcc17121671245b6ffff4bbc1964192be72e74c
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-12-13 16:08:06 +0300

    new branch

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
-import gltf
+from panda3d.physics import *
 
 
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        node = NodePath("PhysicsNode")
+
+        # Model Init
         self.scene = self.loader.loadModel("models/roads2.obj")
         self.car = self.loader.loadModel("models/car.gltf")
+        self.car.ls()
+
+        # Texture Init
         self.car_texture = self.loader.loadTexture("sprites/game.png")
+
+        # Set Texture
         self.car.set_texture(self.car_texture, 1)
-        node.reparentTo(self.render)
+
+        # ReparentTo
         self.scene.reparentTo(self.render)
         self.car.reparentTo(self.render)
-        self.car.ls()
-        self.scene.setScale(0.25, 0.25, 5)
-        self.scene.setPos(0, 6.5, -1)
-        self.scene.setHpr(90, 0, 90)
+
+        # Scale
+        self.scene.setScale(0.26, 0.26, 5)
         self.car.setScale(0.25, 0.25, 0.25)
-        node.physicsMgr.attachPhysicalNode(self.car)
-        self.car.setPos(0, 6.5, -0.5)
+        # Pos
+        self.scene.setPos(0, 6.5, -1)
+        self.car.setPos(0, 6.5, -0.74)
+        # Rotation
+        self.car.setHpr(90, 0, 90)
+        self.scene.setHpr(90, 0, 90)
+
+        # Movement
+
+    def input(self):
+        self.accept("a", self.movement(-1))
+        self.accept("d", self.movement(1))
+
+    def movement(self, x):
+        print(x)
 
 if __name__ == '__main__':
     game = Game()
