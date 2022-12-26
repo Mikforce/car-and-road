93a3889d8c9a42af90576f4401964e3a4f14a9af
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-12-26 14:34:14 +0300

    new branch

--- entities.py
+++ entities.py
@@ -1,10 +1,29 @@
 from panda3d.core import Vec3, Vec2
 from direct.actor.Actor import Actor
+from direct.showbase.ShowBase import ShowBase
 from panda3d.core import CollisionSphere, CollisionNode
+import random
 
 class GameObject():
-    def __init__(self, pos, speed, chance_to_spawn):
-        self.actor = Actor("")
+    def __init__(self, pos: Vec3, speed: float, chance_to_spawn_type: int, collider_name, render, model):
+        # Obstacle's object
+        self.actor = Actor(model)
+        self.actor.reparentTo(render)
+        self.actor.setPos(pos)
+        self.type = None
+        # Speed
+        self.speed = speed
 
-class Obstacle(GameObject):
-    pass
+    def movement(self, speed):
+        self.actor.setPos(self.actor.getPos().x - speed, self.actor.getPos().y, self.actor.getPos().z)
+
+    def update(self):
+        self.movement(self.speed)
+
+    def spawn_type(self):
+        pass
+
+class Obstacle(GameObject, ShowBase):
+    def __init__(self, pos, speed, chance_to_spawn, collider_name, render):
+        self.model = self.loader.loadModel("")
+        GameObject.__init__(self, pos, speed, chance_to_spawn, collider_name, render, self.model)
