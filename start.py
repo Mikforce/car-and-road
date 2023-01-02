256e032eea97cd965c0f9fa953cf775518e8be08
oktoberf <75834827+oktoberf@users.noreply.github.com>
2023-01-02 13:33:58 +0300

    new branch

--- start.py
+++ start.py
@@ -1,3 +1,8 @@
 with open("start.bat", 'w') as bat:
     with open("moduls.txt", 'r') as moduls:
         bat.write(f"{moduls.read()}\npause")
+
+def start_game():
+    with open("start.bat", 'w') as bat:
+        with open("moduls.txt", 'r') as moduls:
+            bat.write(f"python game/main.py")
